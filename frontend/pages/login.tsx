af78d94990cb21b6e8c0f9d9f22f088537f2d4ce
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-17 15:56:24 +0900

    login用のページ作成

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,7 +1,37 @@
+import { Button, TextField } from '@material-ui/core';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { login } from '../types/loginType';
 
 const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm<login>();
+
+  const onSubmit = (data: login) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        variant='outlined'
+        {...register('email', {
+          required: true,
+        })}
+      />
+
+      <TextField
+        variant='outlined'
+        {...register('password', {
+          required: true,
+          minLength: 8,
+          maxLength: 16,
+        })}
+      />
+      <Button type='submit' variant='outlined'>
+        submit
+      </Button>
+    </form>
+  );
 };
 
 export default Login;
