4d3931ec2f8c4be44a4601e24734fdd829afb72a
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-17 16:38:25 +0900

    カスタムフック作成

---
+++ frontend/hooks/useRegister.ts
@@ -0,0 +1,23 @@
+import { AuthServiceClient } from '../proto/AuthServiceClientPb';
+import { RegisterRequest } from '../proto/auth_pb';
+import { register } from '../types/registerType';
+
+const useRegister = () => {
+  const onSubmit = (data: register) => {
+    console.log(data);
+  };
+
+  const sendInfo = async ({ email, name, password }: register) => {
+    const client = new AuthServiceClient('http://localhost:8080');
+    const request = new RegisterRequest();
+    request.setEmail(email);
+    request.setName(name);
+    request.setPassword(password);
+    const response = await client.register(request, {});
+    console.log(response);
+  };
+
+  return { onSubmit };
+};
+
+export default useRegister;
