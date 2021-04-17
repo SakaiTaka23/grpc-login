e44e7d80b29284a51cef7bdf4d45d547e8fa0c93
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-17 11:54:50 +0900

    登録用のハンドラー作成

--- backend/handler/AuthHandler.go
+++ backend/handler/AuthHandler.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"backend/database"
+	"backend/domain"
 	pb "backend/proto"
 	"context"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthServiceServer struct {
@@ -13,5 +18,17 @@ func (s *AuthServiceServer) Register(ctx context.Context, r *pb.RegisterRequest)
 	if err := r.Validate(true); err != nil {
 		return nil, err
 	}
-	return &pb.RegisterResponse{Uid: "1"}, nil
+
+	uid := uuid.New().String()
+	password, _ := bcrypt.GenerateFromPassword([]byte(r.GetPassword()), 14)
+	user := domain.User{
+		ID:       uid,
+		Email:    r.GetEmail(),
+		Name:     r.GetName(),
+		Password: password,
+	}
+
+	uid = database.CreateUser(&user)
+
+	return &pb.RegisterResponse{Uid: uid}, nil
 }
