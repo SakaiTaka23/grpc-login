7712670cb2b233a20c36ef5629dd489b1b5e7966
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-17 14:18:32 +0900

    メールからユーザーを取る関数作成

--- backend/database/UserRepository.go
+++ backend/database/UserRepository.go
@@ -8,3 +8,9 @@ func CreateUser(user *domain.User) string {
 	DB.Create(&user)
 	return user.ID
 }
+
+func FindUserFromMail(mail string) *domain.User {
+	var user domain.User
+	DB.Where("email = ?", mail).First(&user)
+	return &user
+}
