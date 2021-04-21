d34c521db8f6d4c3fa01b1dd6c002a12099f0cb3
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-21 19:16:53 +0900

    インジェクションようの関数実装

---
+++ backend/injector/injector.go
@@ -0,0 +1,37 @@
+package injector
+
+import (
+	"backend/entity/repository"
+	"backend/handler/grpchandler"
+	"backend/infrastructure/datastore"
+	"backend/infrastructure/datastore/mysql"
+	"backend/usecase"
+)
+
+func InjectDB() mysql.MySqlHandler {
+	sqlHandler := mysql.Connect()
+	return *sqlHandler
+}
+
+func InjectUserRepository() repository.UserRepository {
+	sqlHandler := InjectDB()
+	return datastore.NewUserRepository(sqlHandler)
+}
+
+func InjectUserUsecase() usecase.UserUsecase {
+	UserRepo := InjectUserRepository()
+	return usecase.NewUserUsecase(UserRepo)
+}
+
+func InjectAuthUsecase() usecase.AuthUsecase {
+	UserRepo := InjectUserRepository()
+	return usecase.NewAuthUsecase(UserRepo)
+}
+
+func InjectUserHandler() grpchandler.UserHandler {
+	return grpchandler.NewUserHandler(InjectUserUsecase())
+}
+
+func InjectAuthHandler() grpchandler.AuthHandler {
+	return grpchandler.NewAuthHandler(InjectAuthUsecase())
+}
