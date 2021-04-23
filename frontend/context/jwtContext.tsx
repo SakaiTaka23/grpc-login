f9e55a7171b7c2b73c6daa4f539603e9cfd6d5b5
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-23 11:09:21 +0900

    jwtをグローバル管理する仕組みを実装

---
+++ frontend/context/jwtContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
+
+type JWTContextState = {
+  jwt: string;
+  setJWT: Dispatch<SetStateAction<string>>;
+  deleteJWT: () => void;
+};
+
+const JWTContext = createContext({} as JWTContextState);
+
+const JWTContextProvider: FC = ({ children }) => {
+  const [jwt, setJWT] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    setJWT(token);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('jwt', jwt);
+  }, [jwt]);
+
+  const deleteJWT = () => {
+    setJWT('');
+  };
+
+  return <JWTContext.Provider value={{ jwt, setJWT, deleteJWT }}>{children}</JWTContext.Provider>;
+};
+
+export { JWTContext, JWTContextProvider };
