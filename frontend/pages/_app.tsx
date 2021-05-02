7c2650ed93a7ba537c9dc8c3a04c29f8082470d8
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-02 16:32:08 +0900

    material uiに対応できるようにした

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import { JWTProvider } from '../context/jwtContext';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <JWTProvider>
       <Component {...pageProps} />
