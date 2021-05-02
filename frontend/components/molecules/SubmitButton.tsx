09655d1371d061e323cf82b43740d388fddb213c
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-02 16:49:58 +0900

    ボタンにクラスを適用

--- frontend/components/molecules/SubmitButton.tsx
+++ frontend/components/molecules/SubmitButton.tsx
@@ -1,9 +1,17 @@
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const SubmitButton = () => {
+  const classes = useStyles();
+
   return (
-    <Button type='submit' variant='outlined'>
+    <Button type='submit' variant='contained' color='primary' fullWidth className={classes.submit}>
       submit
     </Button>
   );
