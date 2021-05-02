ed7955b9b800fccd7d1712e7071746cb2a7c58de
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-02 17:25:24 +0900

    loginページデザイン

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,3 +1,5 @@
+import { Avatar, Container, CssBaseline, Grid, Link, makeStyles, Typography } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import SubmitButton from '../components/molecules/SubmitButton';
@@ -6,24 +8,64 @@ import PasswordInput from '../components/organisms/input/PasswordInput';
 import useLogin from '../hooks/useLogin';
 import { loginForm } from '../types/FormType';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const Login = () => {
-  const { err, onSubmit } = useLogin();
+  const classes = useStyles();
   const methods = useForm<loginForm>();
+  const { err, onSubmit } = useLogin();
 
   return (
-    <>
-      {err ? <h1>{err.message}</h1> : ''}
-
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <EmailInput />
-
-          <PasswordInput />
-
-          <SubmitButton />
-        </form>
-      </FormProvider>
-    </>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          LogIn
+        </Typography>
+        {/* modify */}
+        {err ? <h1>{err.message}</h1> : ''}
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(onSubmit)} className={classes.form}>
+            <EmailInput />
+            <PasswordInput />
+            <SubmitButton />
+            <Grid container>
+              <Grid item xs>
+                <Link href='#' variant='body2'>
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href='#' variant='body2'>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </FormProvider>
+      </div>
+    </Container>
   );
 };
 
